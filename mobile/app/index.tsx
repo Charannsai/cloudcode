db509dc5a88dc348e659b93c354d0d6efce79282
Charannsai <pathurisai31@gmail.com>
2026-03-19 11:37:37 +0530

    github oauth redirect uri changed

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import {
   View, Text, StyleSheet, TouchableOpacity,
-  ActivityIndicator, Dimensions, Linking,
+  ActivityIndicator, Dimensions,
 } from 'react-native'
+import * as Linking from 'expo-linking'
 import { useRouter } from 'expo-router'
 import * as WebBrowser from 'expo-web-browser'
 import { useAuthStore } from '@/store/auth'
