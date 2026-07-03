b504ca86805ac3ccc7e68e5aaa553d9a72fabaff
Charannsai <pathurisai31@gmail.com>
2026-07-03 16:03:14 +0530

    feat: implement onboarding page and screen illustration components with animated UI elements

--- mobile/components/onboarding/OnboardingPage.tsx
+++ mobile/components/onboarding/OnboardingPage.tsx
@@ -66,6 +66,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 20,
+    overflow: 'hidden',
   },
   textWrapper: {
     width: '100%',
