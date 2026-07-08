92bda29a98aaa78ce33494b5820fa53efa205374
Charannsai <pathurisai31@gmail.com>
2026-07-08 18:21:06 +0530

    chore: icons replace ->hugeicons

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -160,7 +160,7 @@ function CustomTabBar({ state, descriptors, navigation }: any) {
                 activeOpacity={0.8}
               >
                 <View style={[styles.fabButton, { backgroundColor: activeColor }]}>
-                  <SvgIcon name="create" size={24} color={isDark ? '#030303' : '#FFFFFF'} />
+                  <SvgIcon name="create" size={18} color={isDark ? '#030303' : '#FFFFFF'} />
                 </View>
               </TouchableOpacity>
             )
@@ -207,7 +207,7 @@ function CustomTabBar({ state, descriptors, navigation }: any) {
               <TabIconWrapper isFocused={isFocused}>
                 <Icon 
                   color={isFocused ? activeColor : inactiveColor} 
-                  size={32} 
+                  size={20} 
                 />
               </TabIconWrapper>
             </TouchableOpacity>
